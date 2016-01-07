540044e8f7217485c8b9c4fd5b37bcedf0d7fdf6
Xavier Drdak <x.drdak@gmail.com>
2016-01-06 19:20:19 -0500

    Added w3c validators. converted to use fat arrows everywhere, because why not? Changed the license to something... uhh... better?

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,32 @@ const gulp = require('gulp');
 const jade = require('gulp-jade');
 const sass = require('gulp-sass');
 const connect = require('gulp-connect');
+const w3cjs = require('gulp-w3cjs');
+const w3ccss = require('gulp-w3c-css');
+const gutil = require('gulp-util');
 
 const dest = './dist';
 const base = './app';
 
 gulp.task('default', ['server', 'watch']);
+gulp.task('serve', ['server', 'watch']);
+gulp.task('build', ['jade', 'sass']);
 
-gulp.task('jade', function() {
+gulp.task('validate', () => {
+   gulp.src(dest + '/*.html')
+  .pipe(w3cjs())
+  .pipe(w3cjs.reporter());
+   
+  gulp.src(dest + '/css/*.css')
+  .pipe(w3ccss())
+  .pipe(gutil.buffer((err, files) => {
+    files.forEach((result) => {
+      console.log(result.contents.toString());
+    });
+  }));
+});
+
+gulp.task('jade', () => {
   gulp.src(base + '/pages/*.jade')
   .pipe(jade({
     pretty: true
@@ -16,24 +35,25 @@ gulp.task('jade', function() {
   .pipe(gulp.dest(dest));
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', () => {
   gulp.src('./app/sass/**/*.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest(dest + '/css'));
 });
 
-gulp.task('server', function() {
+gulp.task('server', () => {
   connect.server({
     root: 'dist',
     livereload: true
   });
 });
 
-gulp.task('reload', function() {
+gulp.task('reload', () => {
   gulp.src('./dist/*.html')
     .pipe(connect.reload());
 });
-gulp.task('watch', function() {
+
+gulp.task('watch', () => {
   gulp.watch(base + '/**/*.jade', ['jade', 'reload']);
   gulp.watch(base + '/**/*.scss', ['sass','reload']);
 });
